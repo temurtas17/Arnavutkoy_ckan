f5b864d39645c1f3696721099ae6a8e38555169a
temurtas17 <mtemurtas1993@gmail.com>
2024-04-15 10:17:40 +0300

    new changes added

--- ckanext/example_theme/plugin.py
+++ ckanext/example_theme/plugin.py
@@ -1,5 +1,6 @@
 import ckan.plugins as plugins
 import ckan.plugins.toolkit as toolkit
+from ckan.common import CKANConfig
 
 
 # import ckanext.example_theme.cli as cli
@@ -9,6 +10,18 @@ import ckan.plugins.toolkit as toolkit
 #     action, auth, validators
 # )
 
+def most_popular_datasets():
+    '''Return a sorted list of the groups with the most datasets.'''
+
+    # Get a list of all the site's groups from CKAN, sorted by number of
+    # datasets.
+    datasets = toolkit.get_action('package_list')(
+        {}, {'sort': 'package_count desc', 'all_fields': True})
+
+    # Truncate the list to the 10 most popular groups only.
+    datasets = datasets[:10]
+
+    return datasets
 
 class ExampleThemePlugin(plugins.SingletonPlugin):
     plugins.implements(plugins.IConfigurer)
@@ -17,15 +30,15 @@ class ExampleThemePlugin(plugins.SingletonPlugin):
     # plugins.implements(plugins.IActions)
     # plugins.implements(plugins.IBlueprint)
     # plugins.implements(plugins.IClick)
-    # plugins.implements(plugins.ITemplateHelpers)
+    plugins.implements(plugins.ITemplateHelpers)
     # plugins.implements(plugins.IValidators)
     
 
     # IConfigurer
 
-    def update_config(self, config_):
-        toolkit.add_template_directory(config_, "templates")
-        toolkit.add_public_directory(config_, "public")
+    def update_config(self, config: CKANConfig):
+        toolkit.add_template_directory(config, "templates")
+        toolkit.add_public_directory(config, "public")
         toolkit.add_resource("assets", "example_theme")
 
     
@@ -51,8 +64,15 @@ class ExampleThemePlugin(plugins.SingletonPlugin):
 
     # ITemplateHelpers
 
-    # def get_helpers(self):
-    #     return helpers.get_helpers()
+    def get_helpers(self):
+        '''Register the most_popular_groups() function above as a template
+        helper function.
+
+        '''
+        # Template helper function names should begin with the name of the
+        # extension they belong to, to avoid clashing with functions from
+        # other extensions.
+        return {'example_theme_most_popular_datasets': most_popular_datasets}
 
     # IValidators
 
